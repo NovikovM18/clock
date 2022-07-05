6a0f1d8d0804ba1e6c8ab1f281e7b3eb0584070c
NovikovM <novikov1807@gmail.com>
2022-07-05 15:11:10 +0300

    v.1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable, Subject, Subscription } from 'rxjs';
+
+import { map, buffer, debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clock';
+  timerSubscription: Subscription = new Subscription;
+  time: number = 0;
+  displayTime: string = ''
+  running: boolean = false;
+  toggleText: string = 'start';
+  obs = new Subject();
+
+  constructor() {}
+  
+  ngOnInit() {
+    const clickStream = this.obs.asObservable();
+    const multiClickStream = clickStream.pipe(
+        buffer(clickStream.pipe(debounceTime(250))),
+        map(list => list.length),
+        filter(x => x === 2)
+    );
+    multiClickStream.subscribe(() => 
+      this.wait()
+    );       
+  }
+
+  formatTime(num: number){
+    let mm = Math.floor((num % 3600)/60).toString();
+    let ss = Math.floor(num % 60).toString();
+    mm = +mm < 10 ? "0" + mm : mm;
+    ss = +ss < 10 ? "0" + ss : ss;
+    return mm + ':' + ss;
+  }
+
+  start(){
+    this.running = true;
+    this.toggleText = 'stop'
+    const timer = new Observable(observer => {
+      observer.next(this.time);
+      const interval = setInterval(() => {
+        this.time += 1;
+        observer.next(this.time);
+      }, 1000);
+      return () => clearInterval(interval);
+    });
+    this.timerSubscription = timer.subscribe();
+  }
+
+  stop(){
+    this.running = false;
+    this.toggleText = 'start'
+    this.timerSubscription.unsubscribe();
+    this.time = 0;
+  }
+
+  toggle(){
+    switch(this.running) {
+      case false:
+        this.start();
+        break;
+      case true:
+        this.stop();
+        break;
+      default:
+        break
+    }
+  }
+
+  wait(){
+    this.running = false;
+    this.toggleText = 'start'
+    this.timerSubscription.unsubscribe();
+    
+  }
+
+  reset(){
+    this.stop();
+    this.time = 0;
+    this.start();
+  }
+
+  ngOnDestroy() {
+    this.timerSubscription.unsubscribe();
+  }
 }
